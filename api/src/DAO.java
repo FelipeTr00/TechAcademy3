182ce1cf7e2bf0a51b710e04846c144a24b315bf
Felipe Morais <morais.alves.felipe@gmail.com>
2024-09-03 23:14:21 -0300

    Atualizando a API

--- api/src/DAO.java
+++ api/src/DAO.java
@@ -1,6 +1,5 @@
 import model.Scene;
 import model.Item;
-import model.User;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -11,8 +10,6 @@ public class DAO {
     private Connection conn;
 
     public DAO() {
-        this.conn = null;
-
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             this.conn = DriverManager.getConnection(
@@ -20,7 +17,7 @@ public class DAO {
                     "root",
                     "123"
             );
-            System.out.println("Conexão com MySQL OK! Link: http://localhost:5150");
+            System.out.println("Conexão com MySQL OK! Link: http://localhost:5150/");
         } catch (ClassNotFoundException e) {
             System.out.println("Erro de Driver.");
             e.printStackTrace();
@@ -30,95 +27,67 @@ public class DAO {
         }
     }
 
-    public List<Scene> findAllScene() throws SQLException {
+    public List<Scene> findScenesByCurrentScene(int currentScene) throws SQLException {
         List<Scene> scenes = new ArrayList<>();
-        String sql = "SELECT * FROM scenes s WHERE scene_id IS NOT NULL;";
+        String sql = "SELECT * FROM scenes WHERE scene_id = ?";
 
-        try (PreparedStatement stmt = conn.prepareStatement(sql);
-             ResultSet rs = stmt.executeQuery()) {
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, currentScene);
 
-            while (rs.next()) {
-                Scene scene = new Scene();
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    Scene scene = new Scene();
+                    scene.setSceneId(rs.getInt("scene_id"));
+                    scene.setTitle(rs.getString("title"));
+                    scene.setTarget(rs.getInt("target"));
+                    scene.setDescription(rs.getString("description"));
+                    scene.setRightCommand(rs.getString("right_command"));
+                    scene.setInventory(rs.getString("inventory"));
 
-                scene.setSceneId(rs.getInt("scene_id"));
-                scene.setTitle(rs.getString("title"));
-                scene.setTarget(rs.getInt("target"));
-                scene.setDescription(rs.getString("description"));
-                scene.setRightCommand(rs.getString("right_command"));
-                scene.setInventory(rs.getString("inventory"));
-
-                scenes.add(scene);
-
+                    scenes.add(scene);
+                }
             }
-            System.out.println("Scenes: " + scenes.size());
-
-        } catch (SQLException e) {
-            System.out.println("Falha na consulta com o banco.");
-            e.printStackTrace();
         }
 
         return scenes;
-
     }
 
-
-    public List<Item> findAllItem() throws SQLException {
+    public List<Item> findItemsByScene(int sceneId) throws SQLException {
         List<Item> items = new ArrayList<>();
-        String sql = "SELECT * FROM items s WHERE item_id IS NOT NULL;";
+        String sql = "SELECT * FROM items WHERE scene_id = ?";
 
-        try (PreparedStatement stmt = conn.prepareStatement(sql);
-             ResultSet rs = stmt.executeQuery()) {
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, sceneId);
 
-            while (rs.next()) {
-                Item item = new Item();
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    Item item = new Item();
+                    item.setItemId(rs.getInt("item_id"));
+                    item.setItem(rs.getString("item"));
+                    item.setDescrItem(rs.getString("descr_item"));
+                    item.setSceneId(rs.getInt("scene_id"));
 
-                item.setItemId(rs.getInt("item_id"));
-                item.setItem(rs.getString("item"));
-                item.setDescrItem(rs.getString("descr_item"));
-                item.setSceneId(rs.getInt("scene_id"));
-
-                items.add(item);
+                    items.add(item);
+                }
             }
-
-            System.out.println("Items: " + items.size());
-
-        } catch (SQLException e) {
-            System.out.println("Falha na consulta com o banco.");
-            e.printStackTrace();
         }
 
         return items;
-
     }
 
-    public List<User> createUser() throws SQLException {
-        List<User> users = new ArrayList<>();
-        String sql = "INSERT INTO users (name, password) VALUES (?,?);";
+    public Integer getCurrentSceneForUser(String username) throws SQLException {
+        String sql = "SELECT current_scene FROM users WHERE name = ?";
 
-        try (PreparedStatement stmt = conn.prepareStatement(sql);
-             ResultSet rs = stmt.executeQuery()) {
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, username);
 
-            while (rs.next()) {
-                User user = new User();
-
-                user.setId(rs.getInt("user_id"));
-                user.setName(rs.getString("name"));
-                user.setCurrentScene(rs.getInt("current_scene"));
-                user.setPassword(rs.getString("password"));
-
-                users.add(user);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("current_scene");
+                } else {
+                    return null;
+                }
             }
-
-            System.out.println("User: " + users.size());
-
-        } catch (SQLException e) {
-            System.out.println("Falha na consulta com o banco.");
-            e.printStackTrace();
         }
-
-        return users;
     }
-
 }
-
-
--- api/src/Main.java
+++ api/src/Main.java
@@ -1,7 +1,6 @@
 import com.google.gson.Gson;
 import model.Item;
 import model.Scene;
-import model.User;
 import spark.Spark;
 
 import java.sql.SQLException;
@@ -12,28 +11,80 @@ import java.util.Map;
 public class Main {
 
     private static final Gson json = new Gson();
+    private static DAO DAO;
 
     public static void main(String[] args) throws SQLException {
 
-        Spark.port(5150);
-        DAO userDAO = new DAO();
-        DAO scenesDAO = new DAO();
-        DAO itemsDAO = new DAO();
-
-        List<User> users = userDAO.createUser();
-        List<Scene> scenes = scenesDAO.findAllScene();
-        List<Item> items = itemsDAO.findAllItem();
-
-        Map<String, Object> combinedData = new HashMap<>();
-        combinedData.put("scenes", scenes);
-        combinedData.put("items", items);
-
-        String combinedJson = json.toJson(combinedData);
+        Spark.setPort(5150);
+        DAO = new DAO();
 
         Spark.get("/", (req, res) -> {
-            res.type("application/json");
-            return combinedJson;
+            String username = "root";
+
+            Integer currentScene;
+            List<Scene> scenes;
+            List<Item> items;
+
+            try {
+                currentScene = DAO.getCurrentSceneForUser(username);
+
+                if (currentScene != null) {
+                    scenes = DAO.findScenesByCurrentScene(currentScene);
+                    items = DAO.findItemsByScene(currentScene);
+
+                    Map<String, Object> responseData = new HashMap<>();
+                    responseData.put("scenes", scenes);
+                    responseData.put("items", items);
+
+                    res.type("application/json");
+                    return json.toJson(responseData);
+                }
+                else return json.toJson(new ErrorResponse("Usuário não encontrado."));
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+
+                return json.toJson(new ErrorResponse("Erro de conexão MySQL."));
+            }
+        });
+
+        Spark.get("/:username", (req, res) -> {
+            String username = req.params(":username");
+            Integer currentScene;
+
+            try {
+                currentScene = DAO.getCurrentSceneForUser(username);
+            } catch (SQLException e) {
+                e.printStackTrace();
+
+                return json.toJson(new ErrorResponse("Erro de conexão MySQL."));
+            }
+
+            if (currentScene != null) {
+                List<Scene> scenes = DAO.findScenesByCurrentScene(currentScene);
+                List<Item> items = DAO.findItemsByScene(currentScene);
+
+                Map<String, Object> responseData = new HashMap<>();
+                responseData.put("scenes", scenes);
+                responseData.put("items", items);
+
+                res.type("application/json");
+                return json.toJson(responseData);
+            }
+            else return json.toJson(new ErrorResponse("Usuário não encontrado."));
+
         });
     }
-}
 
+    private static class ErrorResponse {
+        private String message;
+
+        public ErrorResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}
