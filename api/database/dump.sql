7a4b5062a2a9979dd57332e4b064a1e424ae5d4d
FelipeTr00 <morais.alves.felipe@gmail.com>
2024-09-04 22:05:21 -0300

    update DAO and Main

--- api/database/dump.sql
+++ api/database/dump.sql
@@ -11,6 +11,7 @@ CREATE TABLE scenes (
                         inventory VARCHAR(255) NOT NULL
 );
 
+-- DROP TABLE users;
 CREATE TABLE users (
                        user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
                        name VARCHAR(255) UNIQUE NOT NULL,
