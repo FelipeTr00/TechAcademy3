13bd2b122f5a0bc722da73d0770e6f230b588d26
mrs <mrs@debian>
2024-09-17 22:44:48 -0300

    Incluindo Play.java, atualizando Main e pacotes

--- api/src/Main.java
+++ api/src/Main.java
@@ -1,209 +1,92 @@
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
-import model.Item;
-import model.Scene;
 import model.User;
 import spark.Request;
 import spark.Response;
 import spark.Spark;
-
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
 import static spark.Spark.*;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
 
 public class Main {
 
     private static final Gson json = new Gson();
     private static DAO DAO;
+    private static Play play;
 
     public static void main(String[] args) {
 
-        Spark.setPort(5150);
+        Spark.port(5150);
 
-
-        Spark.before((req, res) -> {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-            res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-        }); // Tentando corrigir ploblema de acesso no navegador, não deu certo ainda...
+        Spark.before((_, res) -> {
+            res.header("Access-Control-Allow-Origin",
+                    "*");
+            res.header("Access-Control-Allow-Methods",
+                    "GET, POST, PUT, DELETE, OPTIONS");
+            res.header("Access-Control-Allow-Headers",
+                    "Content-Type, Authorization, X-Requested-With");
+        });
 
         DAO = new DAO();
+        play = new Play(DAO);
 
-        get("/", (req, res) -> {
-
-            String username = "root";
-            Integer currentScene;
-            List<Scene> scenes;
-            List<Item> items;
-
-            try {
-                currentScene = DAO.getCurrentSceneForUser(username);
-
-                if (currentScene != null) {
-                    scenes = DAO.findScenesByCurrentScene(currentScene);
-                    items = DAO.findItemsByScene(currentScene);
-
-                Map<String, Object> responseApi = new HashMap<>();
-                responseApi.put("scenes", scenes);
-                responseApi.put("items", items);
-
-                res.type("application/json");
-                    return json.toJson(responseApi);
-                }
-                else {
-                    return json.toJson(new ErrorResponse("Usuário não encontrado."));
-                }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-                return json.toJson(new ErrorResponse("Erro de conexão MySQL."));
-            }
+        get("/", (_, res) -> {
+            res.type("application/json");
+            return play.getScenesAndItems("root");
         });
 
         get("/:scene", (req, res) -> {
             String scene = req.params(":scene");
-            Integer currentScene;
-
-            try {
-                currentScene = DAO.getCurrentSceneForUser(scene);
-                if (currentScene != null) {
-                List<Scene> scenes = DAO.findScenesByCurrentScene(currentScene);
-                List<Item> items = DAO.findItemsByScene(currentScene);
-
-                Map<String, Object> responseApi = new HashMap<>();
-                responseApi.put("scenes", scenes);
-                responseApi.put("items", items);
-
-                res.type("application/json");
-                    return json.toJson(responseApi);
-                }
-                else {
-                    return json.toJson(new ErrorResponse("Usuário não encontrado."));
-                }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-                return json.toJson(new ErrorResponse("Erro de conexão MySQL."));
-            }
+            res.type("application/json");
+            return play.getScenesAndItems(scene);
         });
 
-        // INSERIR USUÁRIO - ajustar Json e CORS 
         post("/insert-user", (req, res) -> {
             try {
                 User user = json.fromJson(req.body(), User.class);
 
                 if (user.getName() == null || user.getPassword() == null) {
                     res.status(400);
-                    return json.toJson(new ErrorResponse("Nome ou senha inválido."));
+                    return json.toJson(new Play.ErrorResponse("Erro ao criar usuário."));
                 }
 
                 DAO.insertUser(user);
 
                 res.status(201);
-                return json.toJson(new SuccessResponse("Usuário criado com sucesso."));
-            } catch (JsonSyntaxException e) {
-                res.status(400);
-                return json.toJson(new ErrorResponse("JSON inválido."));
-            } catch (SQLException e) {
-                e.printStackTrace();
-                res.status(500);
-                return json.toJson(new ErrorResponse("Erro ao criar usuário."));
+                return json.toJson( new Play.SuccessResponse("Usuário criado com sucesso."));
             }
+                catch (SQLException e) {
+                    return json.toJson(new Play.ErrorResponse("Erro ao criar usuário."));
+                }
         });
 
         post("/login", Main::handle);
 
         post("/execute-command", (req, res) -> {
-            try {
-                Map<String, String> commandJson = json.fromJson(req.body(), Map.class);
-                String command = commandJson.get("command");
+            Type mapType = new TypeToken<Map<String, String>>(){}.getType(); // Verificar se está certo
+            Map<String, String> commandJson = json.fromJson(req.body(), mapType);
 
-                if (command == null || command.isEmpty()) {
-                    res.status(400);
-                    return json.toJson(new ErrorResponse("Insira um comando."));
-                }
-
-                String[] commandUse = command.split(" ");
-
-                // COMANDO USE <- ATUALIZAR CHECK E START
-                if (commandUse[0].equalsIgnoreCase("use")) {
-                    String rightCommand = command.substring(command.indexOf(" ") + 1).trim();
-                    int targetScene = DAO.getTargetScene(0, rightCommand);
-
-                    if (targetScene == -1) {
-                        res.status(404);
-                        return json.toJson(new ErrorResponse("Tente outro comando."));
-                    }
-
-                    Map<String, Object> responseApi = new HashMap<>();
-                    responseApi.put("target", targetScene);
-
-                    res.type("application/json");
-                    return json.toJson(responseApi);
-                } else {
-                    res.status(400);
-                    return json.toJson(new ErrorResponse("Tente outro comando."));
-                }
-            } catch (JsonSyntaxException e) {
-                res.status(400);
-                return json.toJson(new ErrorResponse("Erro no JSON."));
-            } catch (SQLException e) {
-                e.printStackTrace();
-                res.status(500);
-                return json.toJson(new ErrorResponse("Erro no banco de dados."));
-            }
+            String command = commandJson.get("command");
+            res.type("application/json");
+            return play.executeCommand(command);
         });
 
     }
 
-    private static Object handle(Request req, Response res) {
-        try {
-            Map<String, String> credentials = json.fromJson(req.body(), Map.class);
-                    String username = credentials.get("username");
-                    String password = credentials.get("password");
+    private static Object handle(Request req, Response res) throws SQLException {
+        Map<String, String> credentials = json.fromJson(req.body(), Map.class);
 
-            if (username == null || password == null) {
-                res.status(400);
-                return json.toJson(new ErrorResponse("Erro de login."));
-            }
+        String username = credentials.get("username");
+        String password = credentials.get("password");
 
-            int currentScene = DAO.getCurrentScene(username, password);
-            res.status(200);
-            return "{\"current_scene\": " + currentScene + "}";
-
-        } catch (SQLException e) {
-            res.status(401);
-            return json.toJson(new ErrorResponse("Erro de login."));
-        } catch (JsonSyntaxException e) {
+        if (username == null || password == null) {
             res.status(400);
-            return json.toJson(new ErrorResponse("JSON inválido."));
+            return json.toJson(new Play.ErrorResponse("Erro de login."));
         }
+
+        int currentScene = DAO.getCurrentScene(username, password);
+        res.status(200);
+        return "{\"current_scene\": " + currentScene + "}";
     }
-
-            private static class ErrorResponse {
-                private String message;
-
-                public ErrorResponse(String message) {
-                    this.message = message;
-                }
-
-                public String getMessage() {
-                    return message;
-                }
-            }
-
-            private static class SuccessResponse {
-                private String message;
-
-                public SuccessResponse(String message) {
-                    this.message = message;
-                }
-
-                public String getMessage() {
-                    return message;
-                }
-            }
 }
--- api/src/Play.java
+++ api/src/Play.java
@@ -1,3 +1,95 @@
+import com.google.gson.Gson;
+import model.Scene;
+import model.Item;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// ########## CLASSE PARA AÇÕES DO JOGO ##########
 public class Play {
 
+    private static final Gson json = new Gson();
+    private static DAO DAO;
+
+    public Play(DAO dao) {
+        DAO = dao;
+    }
+
+    public String getScenesAndItems(String username) {
+        Integer currentScene;
+        List<Scene> scenes;
+        List<Item> items;
+
+        try {
+            currentScene = DAO.getCurrentSceneForUser(username);
+
+            if (currentScene != null) {
+            scenes = DAO.findScenesByCurrentScene(currentScene);
+            items = DAO.findItemsByScene(currentScene);
+
+            Map<String, Object> responseApi = new HashMap<>();
+            responseApi.put("scenes", scenes);
+            responseApi.put("items", items);
+
+            return json.toJson(responseApi);
+            }
+            else return json.toJson(new ErrorResponse("Usuário não encontrado."));
+
+        } catch (SQLException e) {
+            return json.toJson(new ErrorResponse("Erro de conexão MySQL."));
+        }
+    }
+
+    public String executeCommand(String command) {
+        try {
+            if (command == null || command.isEmpty()) {
+            return json.toJson(new ErrorResponse("Insira um comando."));
+            }
+            String[] commandUse = command.split(" ");
+
+            if (commandUse[0].equalsIgnoreCase("use")) {
+            String rightCommand = command.substring(command.indexOf(" ") + 1).trim();
+                int targetScene = DAO.getTargetScene(0, rightCommand);
+
+                if (targetScene == -1) {
+                    return json.toJson(new ErrorResponse("Tente outro comando."));
+                }
+
+            Map<String, Object> responseApi = new HashMap<>();
+            responseApi.put("target", targetScene);
+
+                return json.toJson(responseApi);
+            } else return json.toJson(new ErrorResponse("Tente outro comando."));
+
+        } catch (SQLException e) {
+            return json.toJson(new ErrorResponse("Erro no banco de dados."));
+        }
+    }
+
+//  ######## OPÇÕES DE RESPOSTAS PARA OS ERROS API, CORS E BANCO DE DADOS ##########
+
+        public static class ErrorResponse {
+        private String message;
+
+        public ErrorResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    public static class SuccessResponse {
+        private String message;
+
+        public SuccessResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
 }
