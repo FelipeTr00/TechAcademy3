d4619590f79d975e7d58e818ae7dacfe40ba8e8d
mrs <mrs@debian>
2024-09-25 00:29:44 -0300

    Add Inventory

--- js/main.js
+++ js/main.js
@@ -122,102 +122,94 @@ function createList(items, parentId, updateTitle) {
     //     }
     // }
 
-    let currentSceneId = null; // Inicializa o currentSceneId como null para evitar problemas
+    let currentSceneId = null;
 
-async function executeCommand() {
-    let command = document.getElementById('command-input').value.trim().toLowerCase();
+    async function executeCommand() {
+        let command = document.getElementById('command-input').value.trim().toLowerCase();
+        
+        switch (command) {
+            case 'check itens':
+                document.querySelector('.items').style.display = 'block';
+                return;
+            case 'inventory':
+                if (currentSceneId === null) {
+                    alert('Erro.');
+                    return;
+                }
+                try {
+                    const response = await fetch(`http://localhost:5150/${currentSceneId}`);
+                    if (!response.ok) {
+                        throw new Error('Erro');
+                    }
     
-    switch (command) {
-        case 'check itens':
-            // Exibe os itens relacionados na interface
-            document.querySelector('.items').style.display = 'block';
-            return;  // Finaliza o comando sem continuar a execução
-        case 'inventory':
-            // Verifica se currentSceneId está corretamente definido
-            if (currentSceneId === null) {
-                alert('Erro: Cena atual não definida.');
+                    const data = await response.json();
+    
+                    if (data.scenes[0] && data.scenes[0].inventory && data.scenes[0].inventory.trim() !== "") {
+                        alert(`Inventário: ${data.scenes[0].inventory}`);
+                    } else {
+                        alert('O inventário está vazio.');
+                    }
+    
+                } catch (error) {
+                    console.error('Erro:', error);
+                    alert('Erro.');
+                }
+                return;
+            default:
+                document.querySelector('.items').style.display = 'none';
+                break;
+        }
+    
+        try {
+            const response = await fetch('http://localhost:5150/execute-command', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ command: command })
+            });
+    
+            if (!response.ok) {
+                throw new Error('Erro na resposta da API');
+            }
+    
+            const data = await response.json();
+    
+            if (data.error) {
+                console.error('Erro:', data.error);
                 return;
             }
-            // Busca e exibe o inventário da cena atual
-            try {
-                const response = await fetch(`http://localhost:5150/${currentSceneId}`);
-                if (!response.ok) {
-                    throw new Error('Erro ao buscar os dados da cena');
-                }
-
-                const data = await response.json();
-
-                // Verifica o campo inventory e exibe em um alert se não estiver vazio
-                if (data.scenes[0] && data.scenes[0].inventory && data.scenes[0].inventory.trim() !== "") {
-                    alert(`Inventário: ${data.scenes[0].inventory}`);
-                } else {
-                    alert('O inventário está vazio.');
-                }
-
-            } catch (error) {
-                console.error('Erro ao buscar inventário:', error);
-                alert('Erro ao carregar o inventário.');
+    
+            const scene = data.target;
+            currentSceneId = scene;
+            fetchData(scene);
+    
+        } catch (error) {
+            console.error('Erro ao executar o comando:', error);
+        }
+    }
+    
+    async function fetchData(scene) {
+        try {
+            const response = await fetch(`http://localhost:5150/${scene}`);
+            if (!response.ok) {
+                throw new Error('Erro 404');
             }
-            return;  // Finaliza a execução após mostrar o inventário
-        default:
-            // Se não for "check itens" nem "inventory", esconde a lista de itens
-            document.querySelector('.items').style.display = 'none';
-            break;
+            const data = await response.json();
+    
+            createList(data.scenes, 'scene-list', true);
+            createList(data.items, 'items-list', false);
+    
+            currentSceneId = data.scenes[0].sceneId;
+    
+            if (data.scenes[0] && data.scenes[0].inventory && data.scenes[0].inventory.trim() !== "") {
+                console.log(`Inventário: ${data.scenes[0].inventory}`);
+            } else {
+                console.log('Inventário está vazio.');
+            }
+    
+        } catch (error) {
+            console.error('Erro ao exibir conteúdo', error);
+        }
     }
-
-    // Caso o comando não seja "check itens" ou "inventory", continua o fluxo normal
-    try {
-        const response = await fetch('http://localhost:5150/execute-command', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ command: command })
-        });
-
-        if (!response.ok) {
-            throw new Error('Erro na resposta da API');
-        }
-
-        const data = await response.json();
-
-        if (data.error) {
-            console.error('Erro:', data.error);
-            return;
-        }
-
-        const scene = data.target;
-        currentSceneId = scene; // Atualiza o currentSceneId para a nova cena
-        fetchData(scene);  // Atualiza a cena e os itens
-
-    } catch (error) {
-        console.error('Erro ao executar comando:', error);
-    }
-}
-
-async function fetchData(scene) {
-    try {
-        const response = await fetch(`http://localhost:5150/${scene}`);
-        if (!response.ok) {
-            throw new Error('Erro 404');
-        }
-        const data = await response.json();
-
-        // Atualiza as listas de cenas e itens na interface
-        createList(data.scenes, 'scene-list', true);
-        createList(data.items, 'items-list', false);
-
-        // Atualiza a variável global para armazenar a cena atual
-        currentSceneId = data.scenes[0].sceneId; // Certifique-se de que currentSceneId seja atualizado com o ID correto da cena
-
-        // Verifica o campo inventory e exibe em um alert se não estiver vazio
-        if (data.scenes[0] && data.scenes[0].inventory && data.scenes[0].inventory.trim() !== "") {
-            console.log(`Inventário: ${data.scenes[0].inventory}`);
-        } else {
-            console.log('Inventário está vazio.');
-        }
-
-    } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
-    }
-}
+    
